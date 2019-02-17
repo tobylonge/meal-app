fc3ebf757252acb1a58d3be326fd587b3d0cab6a
Oluwatobi Alonge <tobylonge@gmail.com>
2019-02-17 20:38:56 +0100

    added body parser

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const PORT = 9000;
 
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
     return res.send('The API is working');
 })
